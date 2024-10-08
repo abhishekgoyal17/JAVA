ad14ade29b3270d36a5e81510392d604525ac50c
abhishekgoyal <abhigoya@cisco.com>
2024-10-08 16:54:01 +0530

    priority in threads

--- src/main/java/multithreading/priority/MyThread2.java
+++ src/main/java/multithreading/priority/MyThread2.java
@@ -13,7 +13,7 @@ public class MyThread2 extends Thread{
 
             System.out.println(Thread.currentThread().getName()+"-Priority:"+Thread.currentThread().getPriority()+"-count:"+ i);
             try {
-                Thread.sleep(100);
+                Thread.sleep(200);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -23,11 +23,17 @@ public class MyThread2 extends Thread{
     public static void main(String[] args) {
 
         MyThread2 l=new MyThread2("LOW PRIORITY");
-        MyThread2 m=new MyThread2("LOW PRIORITY");
-        MyThread2 n=new MyThread2("LOW PRIORITY");
+        MyThread2 m=new MyThread2("MEDIUM PRIORITY");
+        MyThread2 n=new MyThread2("HIGH PRIORITY");
 
-        t2.setPriority(Thread.MIN_PRIORITY);
-        t2.start();
+        l.setPriority(Thread.MIN_PRIORITY);
+        m.setPriority(Thread.NORM_PRIORITY);
+        n.setPriority(Thread.MAX_PRIORITY);
+
+        l.start();
+        m.start();
+        n.start();
+
 
     }
 }
