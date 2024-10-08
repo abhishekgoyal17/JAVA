b7f21f282eb8154f8cad41233128a01042fc7631
abhishekgoyal <abhigoya@cisco.com>
2024-10-08 17:23:35 +0530

    daemon threads

---
+++ src/main/java/multithreading/Daemon/MyThread5.java
@@ -0,0 +1,23 @@
+package multithreading.Daemon;
+
+public class MyThread5 extends Thread {
+    @Override
+    public void run() {
+       while(true){
+           System.out.println("Hello world");
+       }
+    }
+
+    public static void main(String[] args) {
+
+        MyThread5 t1= new MyThread5();
+        MyThread5 t2= new MyThread5();
+        t1.setDaemon(true);
+        t1.start();
+        t2.start();
+        System.out.println("main done");
+    }
+}
+
+//DAEMON THREADS
+//jvm doesnot wait for daemon
