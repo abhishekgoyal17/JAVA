0da81f3a2ba4316e973976f64e3f30620530c2e5
abhishekgoyal <abhigoya@cisco.com>
2024-10-11 00:24:16 +0530

    spring

---
+++ Springboot/SpringbootInterviewEssential/src/main/java/com/example/SpringbootInterviewEssential/SpringbootInterviewEssentialApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootInterviewEssential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterviewEssentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInterviewEssentialApplication.class, args);
+	}
+
+}
---
+++ Springboot/SpringbootInterviewEssential/src/main/java/com/example/SpringbootInterviewEssential/controller/AccountsController.java
@@ -0,0 +1,16 @@
+package com.example.SpringbootInterviewEssential.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+
+
+    @GetMapping("sayHello")
+    public String sayHello(){
+        return "Hello world";
+    }
+}
